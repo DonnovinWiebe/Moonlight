30def717f6de64873ccf9dd6111733edd3ef1f47
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-06-16 23:00:41 -0400

    The basic edits that I want to build first.

---
+++ src/processor/edit.rs
@@ -0,0 +1,23 @@
+pub enum Edit {
+    Rotate(f64), // in degrees
+    Crop(Point, Point), // bounding box start, bounding box size
+    Exposure(f64),
+    Brightness(f64),
+    Contrast(f64),
+    Whites(f64), // lightens the whole image, but focuses on the extremes
+    Blacks(f64), // darkens the whole image, but focuses on the extremes
+    Highlights(f64), // lightens mid-light regions
+    Shadows(f64), // darkens mid-dark regions
+    WhitePoint(f64), // adjusts where colors are clipped to max white and regradients image
+    BlackPoint(f64), // adjusts where colors are clipped to max black and regradients the image
+}
+
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+impl Point {
+    pub fn new(x: u32, y: u32) -> Point {
+        Point { x, y }
+    }
+}

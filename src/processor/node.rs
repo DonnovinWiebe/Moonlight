c4ff4587be123fd5f0dc3abdabe3b95afce1b706
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-06-22 22:22:05 -0400

    Improved the node tree system.

--- src/processor/node.rs
+++ src/processor/node.rs
@@ -71,34 +71,121 @@ impl Pool {
 
     // tree management
     pub fn add_node(&mut self, operation: Operation) -> Schrod<()> {
+        // immediately fails if there is no source image
         if self.source_image.is_none() {
             return Schrod::new_fail("No source image found!", "Pool::add_node()")
                 .fail("Failed to add node.", "Pool::add_node()")
         }
 
+        // checks if the pool has a position set
         match self.position {
+            // creates a node downstream of the current location
             Some(position) => {
-                let parent_node_result = self.get(position);
-                if parent_node_result.is_fail() {
-                    return parent_node_result
+                // gets the node at the position
+                let current_node_result = self.get(position);
+                if current_node_result.is_fail() {
+                    return current_node_result
                         .convert("Pool::add_node()")
                         .fail("Failed to add node.", "Pool::add_node()")
                 }
-                let parent_node = parent_node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::add_node()");
-                let new_node = Node::new(Some(parent_node.get_id()), parent_node.get_branch_id(), operation);
+                let current_node = current_node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::add_node()");
+
+                // if the current node already has children, a new branch is created
+                let branch_id = if current_node.has_child() { Uuid::now_v7() } else { current_node.get_branch_id() };
+
+                // creates a new node and sets the current position to the new node
+                let new_node = Node::new(Some(current_node.get_id()), branch_id, operation);
                 self.position = Some(new_node.get_id());
                 self.nodes.push(new_node);
             }
 
+            // creates a node downstream of the base image
             None => {
+                // creates a new node and sets the current position to the new node
                 let new_node = Node::new(None, Uuid::now_v7(), operation);
                 self.position = Some(new_node.get_id());
                 self.nodes.push(new_node);
             }
         }
 
+        // returns a success
+        Pass(())
+    }
 
+    pub fn edit_node(&mut self, node_id: Uuid, new_operation: Operation) -> Schrod<()> {
+        let node_result = self.get_mut(node_id);
+        if node_result.is_fail() {
+            return node_result
+                .convert("Pool::edit_node()")
+                .fail("Failed to edit node.", "Pool::edit_node()")
+        }
+        let node = node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::edit_node()");
+        node.edit_operation(new_operation);
 
+        Pass(())
+    }
+
+    pub fn remove_node(&mut self, node_id: Uuid) -> Schrod<()> {
+        // the node being removed
+        let node_result = self.get_mut(node_id);
+        if node_result.is_fail() {
+            return node_result
+                .convert("Pool::remove_node()")
+                .fail("Failed to remove node.", "Pool::remove_node()")
+        }
+        let node = node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::remove_node()");
+
+        // adding the node's children to the node's parent and setting the children's parents as the nodes parent
+        match node.get_parent_id() {
+            // if the node has a parent
+            Some(parent_id) => {
+                // gets the parent
+                let parent_node_result = self.get_mut(parent_id);
+                if parent_node_result.is_fail() {
+                    return parent_node_result
+                        .convert("Pool::remove_node()")
+                        .fail("Failed to remove node.", "Pool::remove_node()")
+                }
+                let parent_node = parent_node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::remove_node()");
+
+                // updates the node's parent to have all the node's children
+                let children_ids = node.get_children_ids();
+                parent_node.add_children(children_ids.clone());
+                
+                // updates all the node's children to have the node's parent as their parent
+                children_ids.into_iter().for_each(|id| {
+                    let child_node_result = self.get_mut(id);
+                    if child_node_result.is_fail() {
+                        return child_node_result
+                            .convert("Pool::remove_node()")
+                            .fail("Failed to remove node.", "Pool::remove_node()")
+                    }
+                    let child_node = child_node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::remove_node()");
+                    child_node.set_parent(Some(parent_id));
+                });
+            }
+
+            // if the node has no parent
+            None => {
+                // updates all the node's children to have the node's parent (the pool) as their parent
+                let children_ids = node.get_children_ids();
+                children_ids.into_iter().for_each(|id| {
+                    let child_node_result = self.get_mut(id);
+                    if child_node_result.is_fail() {
+                        return child_node_result
+                            .convert("Pool::remove_node()")
+                            .fail("Failed to remove node.", "Pool::remove_node()")
+                    }
+                    let child_node = child_node_result.wont_fail("This is past an is_fail() guard clause.", "Pool::remove_node()");
+                    child_node.set_parent(None);
+                });
+            }
+        }
+
+        // removing the node
+        self.nodes.retain(|existing_node| existing_node !=  node);
+
+        // succeeds
         Pass(())
     }
 }
@@ -141,43 +228,9 @@ impl Node {
     
     pub fn get_parent_id(&self) -> Option<Uuid> { self.parent_id }
 
-    /*
-    pub fn get_parent_pool_index(&self, pool: &Pool) -> Schrod<Option<usize>> {
-        match self.parent_id {
-            Some(parent_id) => {
-                for (index, node) in pool.nodes.iter().enumerate() {
-                    if node.id == parent_id {
-                        return Pass(Some(index));
-                    }
-                }
-                Schrod::new_fail("Parent id does not exist in pool!", "Node::get_parent_pool_index()")
-                    .fail("Failed to get parent pool index.", "Node::get_parent_pool_index()")
-            }
-            
-            None => Pass(None)
-        }
-    }
-    */
+    pub fn has_child(&self) -> bool { self.children_ids.len() > 0 }
 
-    /*
-    pub fn get_parent<'a>(&'a self, pool: &'a Pool) -> Schrod<Option<&'a Node>> {
-        match self.parent_id {
-            Some(parent_id) => {
-                let parent_result = pool.get(parent_id);
-                match parent_result {
-                    Pass(parent) => { Pass(Some(parent)) }
-                    Fail(_) => {
-                        return parent_result
-                            .convert("Node::get_parent()")
-                            .fail("Failed to get parent.", "Node::get_parent()")
-                    }
-                }
-            }
-
-            None => Pass(None)
-        }
-    }
-    */
+    pub fn get_children_ids(&self) -> Vec<Uuid> { self.children_ids.clone() }
     
     pub fn get_operation(&self) -> Operation { self.operation }
 
@@ -197,6 +250,35 @@ impl Node {
     }
 
 
+
+    // basic editing
+    fn set_parent(&mut self, parent_id: Option<Uuid>) {
+        self.parent_id = parent_id;
+    }
+    
+    fn add_child(&mut self, child_id: Uuid) {
+        self.children_ids.push(child_id);
+    }
+
+    fn add_children(&mut self, children_ids: Vec<Uuid>) {
+        self.children_ids.extend(children_ids);
+    }
+
+    fn remove_child(&mut self, child_id: Uuid) -> Schrod<()> {
+        if !self.children_ids.contains(&child_id) {
+            return Schrod::new_fail(&format!("{child_id} is not a child of {}!", self.id), "Node::remove_child()")
+                .fail("Failed to remove child.", "Node::remove_child()")
+        }
+        self.children_ids.retain(|id| *id != child_id);
+
+        Pass(())
+    }
+
+    fn edit_operation(&mut self, new_operation: Operation) {
+        self.operation = new_operation;
+    }
+
+    
 
     // working with images
     fn update_image(&mut self, pool: &Pool) -> Schrod<()> {

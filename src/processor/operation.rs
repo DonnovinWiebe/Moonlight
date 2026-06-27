7837c048ab9499c0036b5277882ff323a65b0aca
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-06-27 13:17:48 -0400

    Improved some documentation.

--- src/processor/operation.rs
+++ src/processor/operation.rs
@@ -2,18 +2,30 @@ use image::{ImageBuffer, Rgba};
 
 #[derive(Debug, Copy, Clone)]
 pub enum Operation {
+    /// Only used for the root `Node` of a `Tree`.
     Root,
-    Rotate(f64), // in degrees
-    Crop(Point, Point), // bounding box start, bounding box size
+    /// Rotates an image in degrees.
+    Rotate(f64),
+    /// Crops an image from the top left corner `Point` at the given size (2nd `Point`).
+    Crop(Point, Point),
+    /// Adjusts the exposure of the image.
     Exposure(f64),
+    /// Adjusts the overall brightness of the image.
     Brightness(f64),
+    /// Adjusts the overall contrast of the image.
     Contrast(f64),
-    Whites(f64), // lightens the whole image, but focuses on the extremes
-    Blacks(f64), // darkens the whole image, but focuses on the extremes
-    Highlights(f64), // lightens mid-light regions
-    Shadows(f64), // darkens mid-dark regions
-    WhitePoint(f64), // adjusts where colors are clipped to max white and regradients image
-    BlackPoint(f64), // adjusts where colors are clipped to max black and regradients the image
+    /// Lightens the highlights of the image.
+    Whites(f64),
+    /// Darkens the shadows of the image.
+    Blacks(f64),
+    /// Pulls down the highlights of the image.
+    Highlights(f64),
+    /// Lifts the shadows of the image.
+    Shadows(f64),
+    /// Adjusts the brightness that is considered maximum white (clipping point).
+    WhitePoint(f64),
+    /// Adjusts the brightness that is considered maximum black (clipping point).
+    BlackPoint(f64),
 }
 impl Operation {
     pub fn apply_to(&self, image: &mut ImageBuffer<Rgba<f32>, Vec<f32>>) {
@@ -21,12 +33,16 @@ impl Operation {
     }
 }
 
+/// Defines a 2d point in an image in pixels.
+/// This can also be used to define a size (as in Operation::Crop).
 #[derive(Debug, Copy, Clone)]
 pub struct Point {
     pub x: u32,
     pub y: u32,
 }
 impl Point {
+    /// Returns a new `Point`.
+    #[must_use]
     pub fn new(x: u32, y: u32) -> Point {
         Point { x, y }
     }

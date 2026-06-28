7122b75c375d195a8cf8babc52bc914024193c39
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-06-27 22:52:26 -0400

    Added basic getters and an orchard to the app.

--- src/state/app.rs
+++ src/state/app.rs
@@ -1,39 +1,64 @@
 use iced::{Element, Subscription, Task, Theme, event, widget::Text};
 use materialui::material::MaterialThemes;
 
-use crate::state::signal::Signal;
+use crate::{state::signal::Signal, workspace::tree::Tree};
 
 /// The central application container.
 /// This holds the `Bank` and all ui/ux state information.
 #[allow(clippy::struct_excessive_bools)] // This is more ergonomic than using enums for bool flags.
 pub struct App {
     // basic app state
-    pub material_theme: MaterialThemes,
-    pub application_failures: Vec<String>,
-    pub theme: Theme,
+    material_theme: MaterialThemes,
+    application_failures: Vec<String>,
+    theme: Theme,
+
+    // projects
+    current_tree: Option<usize>,
+    orchard: Vec<Tree>,
 }
 impl App {
     // initializing
     /// Creates a new `App`.
     pub fn new() -> (App, Task<Signal>) {
         let app = App {
+            // basic app state
             material_theme: MaterialThemes::Midnight,
             application_failures: Vec::new(),
             theme: MaterialThemes::Midnight.generate_iced_palette(),
+
+            // projects
+            current_tree: None,
+            orchard: Vec::new(),
         };
         
         (app, Task::done(Signal::StartLoading))
     }
 
+
+
+    // basic getters
     /// The tile of the `App`.
     #[must_use]
     pub fn title(&self) -> String {
         "Moonlight".to_string()
     }
+
+    /// Gets the current `MaterialTheme'.
+    pub fn get_material_theme(&self) -> MaterialThemes {
+        self.material_theme
+    }
     
     /// Gets the current `Theme`.
     pub fn theme(&self) -> Theme {
         self.theme.clone()
+    }
+
+    /// Gets the current `Tree`.
+    pub fn get_current_tree(&self) -> Option<&Tree> {
+        match &self.current_tree {
+            Some(i) => { Some(&self.orchard[*i]) }
+            None => { None }
+        }
     }
 
 
